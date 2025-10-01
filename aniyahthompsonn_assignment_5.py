13bb14061aeb2eadfb36bc4a78962d34e9080fb6
aniyahthompsonn <athompson10@aggies.ncat.edu>
2025-10-01 14:08:04 -0400

    Challenge 2: Prime checker - for loop for known range

--- aniyahthompsonn_assignment_5.py
+++ aniyahthompsonn_assignment_5.py
@@ -17,3 +17,21 @@ while current_number != 1:
 print ()
 print ("Steps:", step_count)
 print ()
+
+
+# CHALLENGE 2
+print ("=== Challenge 2: Prime Number Checker ===")
+
+input_num = int(input("Enter a number: "))
+print (f"Testing divisors from 2 to {input_num - 1}...")
+
+is_prime = True
+
+for i in range (2, input_num):
+    if input_num % i == 0:
+        print (f"{input_num} is not prime (divisible by {i})")
+        is_prime = False
+        break
+
+if is_prime:
+    print (f"{input_num} is prime!\n")
